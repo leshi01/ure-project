ef1d8dc809781b16913ac88c86f6021c573b1afe
Shkelzen <106072048+leshi01@users.noreply.github.com>
2024-04-29 15:19:25 +0300

    seperated the files to be more understundable

--- src/OwnerView/DataGridDisplay.jsx
+++ src/OwnerView/DataGridDisplay.jsx
@@ -2,14 +2,11 @@ import React, { useState } from 'react';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
 import AddIcon from '@mui/icons-material/Add';
+import Agreement from './Agreement';
 import DeleteIcon from '@mui/icons-material/DeleteOutlined';
 import SaveIcon from '@mui/icons-material/Save';
 import IconButton from '@mui/material/IconButton';
 import { DataGrid } from '@mui/x-data-grid';
-import { Modal as BaseModal } from '@mui/material'; 
-import PropTypes from 'prop-types';
-import clsx from 'clsx';
-import { styled, css } from '@mui/system';
 
 
 function DataGridDisplay({ menuItems, menuChange }) {
@@ -75,21 +72,7 @@ function DataGridDisplay({ menuItems, menuChange }) {
 
     return (
         <div className='data-grid'>
-            <Modal
-                aria-labelledby="unstyled-modal-title"
-                aria-describedby="unstyled-modal-description"
-                open={open}
-                onClose={handleClose}
-                slots={{ backdrop: StyledBackdrop }}
-            >
-                <ModalContent sx={{display: "inline-block", width: 350 }}>
-                    <h2 id="unstyled-modal-title" className="modal-title">
-                        Do you agree to delete this element?
-                    </h2>
-                    <Button variant="text" sx={{ minWidth: 170}} onClick={handleDelete}>Yes</Button>
-                    <Button variant="text" sx={{ minWidth: 170}} onClick={handleClose}>No</Button>
-                </ModalContent>
-            </Modal>
+            <Agreement open={open} handleClose={handleClose} handleDelete={handleDelete}/>
             <Box sx={{ height: 500, width: 1000 }}>
                 <Button color="primary" startIcon={<AddIcon />} onClick={handleAdd}>
                     Add record
@@ -106,85 +89,7 @@ function DataGridDisplay({ menuItems, menuChange }) {
 }
 
 
-const Backdrop = React.forwardRef((props, ref) => {
-    const { open, className, ...other } = props;
-    return (
-      <div
-        className={clsx({ 'base-Backdrop-open': open }, className)}
-        ref={ref}
-        {...other}
-      />
-    );
-  });
-  
-  Backdrop.propTypes = {
-    className: PropTypes.string.isRequired,
-    open: PropTypes.bool,
-  };
-  
-  const grey = {
-    50: '#F3F6F9',
-    100: '#E5EAF2',
-    200: '#DAE2ED',
-    300: '#C7D0DD',
-    400: '#B0B8C4',
-    500: '#9DA8B7',
-    600: '#6B7A90',
-    700: '#434D5B',
-    800: '#303740',
-    900: '#1C2025',
-  };
-  
-  const Modal = styled(BaseModal)`
-    position: fixed;
-    z-index: 1300;
-    inset: 0;
-    display: flex;
-    align-items: center;
-    justify-content: center;
-  `;
-  
-  const StyledBackdrop = styled(Backdrop)`
-    z-index: -1;
-    position: fixed;
-    inset: 0;
-    background-color: rgb(0 0 0 / 0.5);
-    -webkit-tap-highlight-color: transparent;
-  `;
-  
-  const ModalContent = styled('div')(
-    ({ theme }) => css`
-      font-family: 'IBM Plex Sans', sans-serif;
-      font-weight: 500;
-      text-align: start;
-      position: relative;
-      display: flex;
-      flex-direction: column;
-      gap: 8px;
-      overflow: hidden;
-      background-color: ${theme.palette.mode === 'dark' ? grey[900] : '#fff'};
-      border-radius: 8px;
-      border: 1px solid ${theme.palette.mode === 'dark' ? grey[700] : grey[200]};
-      box-shadow: 0 4px 12px
-        ${theme.palette.mode === 'dark' ? 'rgb(0 0 0 / 0.5)' : 'rgb(0 0 0 / 0.2)'};
-      padding: 24px;
-      color: ${theme.palette.mode === 'dark' ? grey[50] : grey[900]};
-  
-      & .modal-title {
-        margin: 0;
-        line-height: 1.5rem;
-        margin-bottom: 8px;
-      }
-  
-      & .modal-description {
-        margin: 0;
-        line-height: 1.5rem;
-        font-weight: 400;
-        color: ${theme.palette.mode === 'dark' ? grey[400] : grey[800]};
-        margin-bottom: 4px;
-      }
-    `,
-  );
+
 
 
 export default DataGridDisplay;
