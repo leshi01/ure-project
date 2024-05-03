83ac7b971bf0e2afc8a0a9a82ac9cb65636dd130
Shkelzen <106072048+leshi01@users.noreply.github.com>
2024-05-03 17:02:27 +0300

    small updates

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,6 @@ function App() {
 
   return (
       <div className='main-page'>
-        
         <Routes>
           <Route path="/loginPage" element={<SignIn userLogIn={userLogIn} />} />
           <Route path="/cview" element={<CView menuItems={menuItems} />} />
